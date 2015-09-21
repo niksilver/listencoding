866e9bada035642a5a371099125c6cdca2cd0e0c
Nik Silver <nik@niksilver.com>
2015-09-21 22:46:36 +0100

    Improved output of single digits.

--- src/main/scala/org/pigsaw/listencoding/Encoding.scala
+++ src/main/scala/org/pigsaw/listencoding/Encoding.scala
@@ -1,12 +1,17 @@
 package org.pigsaw.listencoding
 
 case class Encoding(num: Int, freq: Int) {
-  override def toString() = s"($num,$freq)"
+
+  override def toString = freq match {
+    case 1 => num.toString
+    case _ => s"($num,$freq)"
+  }
 }
 
 object Encoding {
+
   def make(s: Seq[Int]): Seq[Encoding] = s.isEmpty match {
-    case true  => Seq()
+    case true => Seq()
     case false =>
       val num = s.head
       val freq = s.prefixLength(_ == num)
--- src/test/scala/org/pigsaw/listencoding/EncodingTest.scala
+++ src/test/scala/org/pigsaw/listencoding/EncodingTest.scala
@@ -14,12 +14,16 @@ class EncodingTest extends FlatSpec with ShouldMatchers {
     Encoding.make(seq) should equal (Seq(Encoding(5,4)))
   }
 
-  it should "give multiple encodings for several repeated digits" in {
+  it should "give repeat encodings for several repeated digits" in {
     val seq = Seq(5,5,6,6,6,5)
     Encoding.make(seq) should equal (Seq(Encoding(5,2), Encoding(6,3), Encoding(5,1)))
   }
 
-  "toString" should "print nicely" in {
+  "toString" should "be nice for a repeat encoding" in {
     Encoding(3,4).toString should equal ("(3,4)")
+  }
+
+  it should "return a single digit for a single encoding" in {
+    Encoding(3,1).toString should equal ("3")
   }
 }
